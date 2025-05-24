121b0263903f80324e372b1176d8c3b5634e76ee
MoizAli4444 <moizali1280@gmail.com>
2025-05-24 23:13:12 +0500

    create object to pass in props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import Hello from "./components/Hello"
-
+import Hello from "./components/Hello";
 
 function App() {
+  // const seatNumbers = [1,3,5];
 
-  const seatNumbers = [1,3,5];
+  const person = {
+    name: "MoizAli4444",
+    message: "Hello there,",
+    seatNumbers: [1, 3, 5],
+  };
 
   return (
     <>
-      <div className='App'>
-       <Hello name='MoizAli4444' message='Hello there,' seatNumbers={seatNumbers}/>
+      <div className="App">
+        {/* <Hello name='MoizAli4444' message='Hello there,' seatNumbers={seatNumbers}/> */}
+        <Hello person={person} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
