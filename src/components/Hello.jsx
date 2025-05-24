b7a206abc660d8e70090f236e7e7e44bcc3832a5
MoizAli4444 <moizali1280@gmail.com>
2025-05-24 23:19:32 +0500

    display object with destructure

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,16 +1,34 @@
-const Hello = (props) => {
-// const Hello = ({name,message}) => {
-
-  // const {message,name} = props;
-
-  // props.name = 'change'; // this show error becaus props are Immutable
+const Hello = ({person}) => {
 
   return (
-    // <div>{props.message} {props.name} {props.seatNumbers} </div>
-
-    // with props
-    <div>{props.person.message} {props.person.name} {props.person.seatNumbers} </div> 
+  
+    // with destructure
+    <div>{person.message} {person.name} {person.seatNumbers} </div> 
   )
 }
 
 export default Hello
+
+
+
+
+
+
+
+
+// const Hello = (props) => {
+// // const Hello = ({name,message}) => {
+
+//   // const {message,name} = props;
+
+//   // props.name = 'change'; // this show error becaus props are Immutable
+
+//   return (
+//     // <div>{props.message} {props.name} {props.seatNumbers} </div>
+
+//     // with props
+//     <div>{props.person.message} {props.person.name} {props.person.seatNumbers} </div> 
+//   )
+// }
+
+// export default Hello
