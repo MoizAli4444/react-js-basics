e3ea585d25c72027f7c15e96cdc88b1fc2980695
MoizAli4444 <moizali1280@gmail.com>
2025-05-24 23:49:31 +0500

    create array of obj

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,5 +1,12 @@
 export default function Fruits() {
-  const fruits = ["Apple", "Mango", "Banana", "Orange", "Pineapple"];
+//   const fruits = ["Apple", "Mango", "Banana", "Banana", "Pineapple"];
+  const fruits = [
+    {name:"Apple", price:30},
+    {name:"Mango", price:30},
+    {name:"Banana", price:90},
+    {name:"Pineapple", price:10},
+    {name:"Banana", price:30},
+];
 
   return (
     <>
