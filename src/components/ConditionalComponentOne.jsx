6dd59368fb0caae531e46986ba4767b6e144b72f
MoizAli4444 <moizali1280@gmail.com>
2025-05-25 11:46:03 +0500

    create conditional components

---
+++ src/components/ConditionalComponentOne.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ConditionalComponentOne = () => {
+  return (
+    <div>ConditionalComponent when value (True)</div>
+  )
+}
+
+export default ConditionalComponentOne
