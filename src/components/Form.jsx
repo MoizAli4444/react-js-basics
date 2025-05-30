a433dcb5d5e790025b4f5ec337836c59cef7c9a3
MoizAli4444 <moizali1280@gmail.com>
2025-05-30 15:00:01 +0500

    Handling Form Submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,17 @@ const Form = () => {
     // const [name,setName] = useState("");
     const [name,setName] = useState({firstName:"", lastName:""});
 
+    function handleSubmit(e){
+      e.preventDefault();
+      console.log(name);
+    }
+
   return (
     <>
         <form action="" method="post">
             <input type="text" name="" id="" onChange={(e) => setName({...name, firstName:e.target.value})} value={name.firstName} />
             <input type="text" name="" id="" onChange={(e) => setName({...name, lastName:e.target.value})} value={name.lastName} />
+            <button onClick={(e)=> handleSubmit(e)}>Submit</button>
         </form>
     </>
   )
